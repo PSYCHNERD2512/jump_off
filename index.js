68d531515e70dc325f4468fb2067a6022a3b58d4
PSYCHNERD2512 <jatinsinghal2003@gmail.com>
2023-08-18 14:17:33 +0530

    first beta

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let boardW = 919;
 let boardH = 403;
 let context;
 
-let playerW = 80;
-let playerH = 180;
+let playerW = 632/8;
+let playerH = 1638/8;
 let playerX = 50;
 let playerY = boardH - playerH;
 let playerImg;
@@ -13,6 +13,7 @@ let player = {
     y: playerY,
     w: playerW,
     h: playerH,
+    animationFrames: [],
 };
 
 let points = 0;
@@ -23,8 +24,9 @@ let stoneX = 800;
 let stoneY = boardH - stoneH;
 let stoneImg;
 let i = 1;
-let req = 13;
+let req = 12;
 let stonearr = [];
+let corr_stone = [1,3,4,6,8,10,12];//Tweak this array according to your expected answers.
 
 let stone_velX = 0;
 let stone_velY = 0;
@@ -35,33 +37,40 @@ let score = 0;
 let rightPressed = false;
 let leftPressed = false;
 
+let animationFrameInterval = 200; 
+
 window.onload = function () {
-  
-    
     board = document.getElementById("board");
     board.height = boardH;
     board.width = boardW;
     context = board.getContext("2d");
-    
+
+    const numFrames = 7;
     playerImg = new Image();
-    playerImg.src = "./img/player.png";
-    playerImg.onload = function () {
-        context.drawImage(playerImg, player.x, player.y, player.w, player.h);
-    };
+    
+    playerImg.src = "./img/player1.png";
+    
+    player.animationFrames = [];
+    for (let frame = 1; frame <= numFrames; frame++) {
+        
+        const frameImg = new Image();
+        frameImg.src = `./img/player${frame}.png`;
+        player.animationFrames.push(frameImg);
+        
+    }
 
     stoneImg = new Image();
     stoneImg.src = "./img/stone.png";
-    
+
     requestAnimationFrame(update);
     setInterval(placeStone, 100);
-    document.addEventListener("keydown", handleKeyDown);
-    document.addEventListener("keyup", handleKeyUp);
+    setInterval(loadAnimationFrames, animationFrameInterval);
+
 };
 
 function handleKeyDown(e) {
     if (game_over) {
-        resetButton = document.getElementById("reset");
-        resetButton.style.visibility = "visible";
+        
         return;
     }
     if (e.code === "Space" || e.code === "ArrowUp") {
@@ -87,15 +96,15 @@ function handleKeyUp(e) {
 }
 
 function update() {
-    if(game_over ==true){
+    if (game_over == true) {
         return;
     }
     requestAnimationFrame(update);
-    context.clearRect(0, 0, boardW, boardH); 
-    
+    context.clearRect(0, 0, boardW, boardH);
+
     stone_velY += gravity;
     player.y = Math.min(player.y + stone_velY, playerY);
-    
+
     if (rightPressed) {
         stone_velX = -12;
     } else if (leftPressed) {
@@ -103,44 +112,58 @@ function update() {
     } else {
         stone_velX = 0;
     }
-    
-    context.drawImage(playerImg, player.x, player.y, player.w, player.h);
-    
+
+    if (rightPressed || leftPressed) {
+        playerImg = player.animationFrames[Math.floor(Date.now() / 100) % player.animationFrames.length];
+    } else {
+        playerImg = player.animationFrames[0];
+    }
+
+    context.save();
+    if (stone_velX > 0) {
+        context.drawImage(playerImg, player.x, player.y, player.w, player.h);
+    } else {
+        context.scale(-1, 1);
+        context.drawImage(playerImg, -player.x - player.w, player.y, player.w, player.h);
+    }
+    context.restore();
+
     for (let j = 0; j < req; j++) {
         let stone = stonearr[j];
         stone.x += stone_velX;
-    
+
         context.drawImage(stone.img, stone.x, stone.y, stone.w, stone.h);
-    
+
         context.fillStyle = "black";
         context.font = "22px bold Arial";
         context.fillText(stone.n, stone.x + stone.w / 2 - 5, stone.y + stone.h / 2 + 5);
-    
-        // Check for collision with the player
+
         if (
             player.x + player.w > stone.x &&
             player.x < stone.x + stone.w &&
             player.y + player.h > stone.y &&
             player.y < stone.y + stone.h
         ) {
-            
-                points++;
-                stone.x = -stone.w; 
-                
-            
+            if (corr_stone.includes(stone.n)) {
+                score+=2;
+            } else {
+                score--;
+            }
+            stone.x = -20000*stone.x;
         }
     }
-    if (stonearr[req - 1].x <= -stonearr[req - 1].w - 25) {
-        game_over = true;
+    const lastStone = stonearr[req - 1];
+
+    if (lastStone.x + lastStone.w+200< player.x) {
+        resetButton = document.getElementById("reset");
+        resetButton.style.visibility = "visible";
     }
     
-    
+
     displayPoints();
-    
 }
 
 function placeStone() {
-    
     if (i <= req) {
         let Stone = {
             x: null,
@@ -150,26 +173,42 @@ function placeStone() {
             h: stoneH,
             w: stoneW,
         };
-        
+
         Stone.x = stoneX + ((i - 1) * 600);
-        
+
         stonearr.push(Stone);
         i++;
     }
 }
 
-
-
-
-function resetGame() {
-   location.reload();
-}
-
 function displayPoints() {
     context.fillStyle = "white";
     context.font = "22px bold Arial";
-    context.fillText("Points: " + points, 10, 30); // Display points at (10, 30) on the canvas
+    context.fillText("Score: " + score, 10, 30);
 }
 
 
+function resetGame() {
+    location.reload();
+}
 
+function loadAnimationFrames() {
+    if (i <= numFrames) {
+        let frameImg = new Image();
+        frameImg.src = `./img/player${i}.png`;
+        player.animationFrames.push(frameImg);
+        i++;
+    }
+}
+function startGame() {
+    const startButton = document.getElementById("start");
+    startButton.style.display = "none"; 
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+    resetButton = document.getElementById("reset");
+        resetButton.style.visibility = "visible";
+}
+function submit() {
+    
+}
